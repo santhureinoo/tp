31c22df9835628bc96d463f253f32075ed4b3b37
San_ThureinOo <santhueinoo@gmail.com>
2023-09-13 10:15:57 +0630

    fix:  updated pill button height

--- components/PillButton.tsx
+++ components/PillButton.tsx
@@ -1,18 +1,19 @@
 interface Props {
     className: string;
+    height?: string;
     isLoading?: boolean;
     text: string;
     onClick?: () => void;
     disabled?: boolean;
 }
 
-const PillButton = ({ className, text, isLoading = false, disabled = false, onClick }: Props) => {
+const PillButton = ({ className, text, isLoading = false, disabled = false, height = "h-8", onClick }: Props) => {
     return (
         <button disabled={disabled} onClick={(event: any) => {
             if (onClick) {
                 onClick();
             }
-        }} className={`rounded-full my-auto h-8 disabled:opacity-50 ${className}`}>
+        }} className={`rounded-full my-auto ${height} disabled:opacity-50 ${className}`}>
             {text}
         </button>
     )
--- pages/Reports.tsx
+++ pages/Reports.tsx
@@ -1017,7 +1017,7 @@ query Date_range_customer_dashboards {
         leftSideElements={[<div key={"table-option"} className='flex flex-row justify-between'>
           <TableOptionField key={uuidv4()} label={'Customer Type'} onChange={(selectedValue: any) => { setSelectedCustomerType(selectedValue); setSelectedSavingsMonth("All"); setSelectedSavingsYear("All") }}
             selectedValue={selectedCustomerType} data={['Group', 'Outlet']} />
-          <PillButton key={'recal'} onClick={globalRecalculate} className={`text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-md text-sm px-5 py-2.5 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 w-40 h-10`} text={"Global Recalculate"}></PillButton>
+          <PillButton key={'recal'} onClick={globalRecalculate} height={"h-10"} className={`text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-md text-sm px-5 py-2.5 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 w-40`} text={"Global Recalculate"}></PillButton>
         </div>, savingsSubMenu()]}
         leftSideFlexDirection={"Vertical"}
         rightSideElements={[]}
