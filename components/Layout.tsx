caf4de522da4fc94b4bd6185c12223c5d2bb8c6c
Santhureinoo <santhueinoo@gmail.com>
2024-02-23 16:45:28 +0630

    fix: reverted layout

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -21,29 +21,30 @@ const Layout = ({ title, children, disableHeader = false, disableSideBar = false
                 {!disableSideBar && <Sidebar setSidebarOpen={setSidebarOpen} sidebarOpen={sidebarOpen} />}
                 <div className="flex-1 flex flex-col w-3/4">
                     <div className="flex-1 overflow-x-hidden overflow-y-auto">
-                        {!disableSideBar && <div className="container mx-auto px-6 py-8">
-                            <button onClick={e => {
+                        <div className="container mx-auto px-6 py-8">
+                            {!disableSideBar && <button onClick={e => {
                                 setSidebarOpen(!sidebarOpen)
                             }} className="text-gray-500 focus:outline-none lg:hidden">
                                 <svg className="h-6 w-6" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                                     <path d="M4 6H20M4 12H20M4 18H11" stroke="currentColor" strokeWidth="2" strokeLinecap="round"
                                         strokeLinejoin="round"></path>
                                 </svg>
-                            </button>
-                            <>
-                                <h3 className="text-gray-700 text-3xl font-bold">{title}</h3>
-                                <div className="flex flex-col mt-8">
-                                    <div className="-my-2 py-2 sm:-mx-6 sm:px-6 lg:-mx-8 lg:px-8">
-                                        <div
-                                            className="align-middle inline-block min-w-full sm:rounded-lg">
-                                            {children}
+                            </button>}
+                            {!disableHeader &&
+                                <>
+                                    <h3 className="text-gray-700 text-3xl font-bold">{title}</h3>
+                                    <div className="flex flex-col mt-8">
+                                        <div className="-my-2 py-2 sm:-mx-6 sm:px-6 lg:-mx-8 lg:px-8">
+                                            <div
+                                                className="align-middle inline-block min-w-full sm:rounded-lg">
+                                                {children}
+                                            </div>
                                         </div>
                                     </div>
-                                </div>
-                            </>
+                                </>
 
-
-                        </div>}
+                            }
+                        </div>
                         {disableHeader && children}
                     </div>
                 </div>
